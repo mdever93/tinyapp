bd32de640cb4a31e2ba34523e076a73a68ae9f37
Michael Dever <mdever@live.ca>
2021-12-08 14:50:49 -0500

    registration template

--- express_server.js
+++ express_server.js
@@ -91,6 +91,12 @@ app.post('/urls/:id/delete', (req, res) => {
   res.redirect('/urls');
 })
 
+app.get('/register', (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+
+  res.render('register', templateVars);
+})
+
 app.post('/login', (req, res) => {
   const username = req.body.username
   res.cookie('username', username);
